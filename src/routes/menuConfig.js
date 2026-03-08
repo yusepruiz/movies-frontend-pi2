f777aba0fed40c5663ac8d3c2c5914af96f05d27
Yusep Ruiz <yusep3012@hotmail.com>
2026-03-07 23:18:03 -0500

    Se crea el menuConfig para centralizar las rutas de los módulos y se renombra el archivo de las rutas de la app de routes a appRoutes

---
+++ src/routes/menuConfig.js
@@ -0,0 +1,9 @@
+import { directorMenu } from "@/modules/director/directorMenu";
+// import { genderMenu } from "@/modules/gender/genderMenu";
+// import { mediaMenu } from "../modules/media/mediaMenu";
+
+export const menuConfig = [
+    directorMenu,
+    // genderMenu,
+    // mediaMenu,
+];
