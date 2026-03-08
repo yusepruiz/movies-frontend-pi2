ac70d3a1afa6735b4f8c2dd07e5429ffef4ca01f
Yusep Ruiz <yusep3012@hotmail.com>
2026-03-07 23:14:49 -0500

    Creación de las opciones del menú para el director

---
+++ src/modules/director/directorMenu.js
@@ -0,0 +1,9 @@
+export const directorMenu = {
+    title: "Directores",
+    id: "menuDirector",
+    icon: "bi-people",
+    links: [
+        { label: "Listar Directores", to: "/director", icon: "bi-list-ul" },
+        { label: "Crear Director", to: "/director/create", icon: "bi-plus-circle" },
+    ],
+};
