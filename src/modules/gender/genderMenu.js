92a225e10beb7e9a0f80d13ec6950b24bc5abbbd
Yusep Ruiz <yusep3012@hotmail.com>
2026-03-10 02:25:47 -0500

    Se crea las opciones del menú lateral

---
+++ src/modules/gender/genderMenu.js
@@ -0,0 +1,9 @@
+export const genderMenu = {
+    title: "Géneros",
+    id: "menuGender",
+    icon: "bi-film",
+    links: [
+        { label: "Listar Géneros", to: "/gender", icon: "bi-list-ul" },
+        { label: "Crear Género", to: "/gender/create", icon: "bi-plus-circle" },
+    ],
+};
