ee47161b6563989c37089b2efc4fe78974f62f16
Yusep Ruiz <yusep3012@hotmail.com>
2026-03-10 12:35:52 -0500

    Se crea el formulario para crear y actualizar el tipo de película

---
+++ src/modules/type/components/TypeForm.jsx
@@ -0,0 +1,59 @@
+import { useTypeForm } from "@/modules/type/hooks/useTypeForm";
+import { FormLayout } from "@/components/layout/FormLayout";
+import { FormInput } from "@/components/common/FormInput";
+import { FormTextArea } from "@/components/common/FormTextArea";
+
+
+/**
+ * Componente de formulario para la creación y actualización de tipos de películas.
+ * Utiliza el hook `useTypeForm` para centralizar la lógica y el estado.
+ * 
+ * @returns {JSX.Element} El formulario de tipos de películas renderizado.
+ */
+export const TypeForm = () => {
+    const {
+        name, setName, description, setDescription,
+        loading, handleSubmit,
+        isEditMode, responseState
+    } = useTypeForm();
+
+    return (
+        <FormLayout
+            title={isEditMode ? "Actualizar Tipo de Película" : "Crear Tipo de Película"}
+            onSubmit={handleSubmit}
+            loading={loading}
+            isEditMode={isEditMode}
+            responseState={responseState}
+            backTo="/type"
+            text="tipos de películas"
+        >
+            {/* Contenedor principal de campos */}
+            <div className="row g-3">
+
+                {/* PRIMERA FILA: Nombre y Switch */}
+                <div className="col-12 col-md-8">
+                    <FormInput
+                        label="Nombre del Tipo de Película"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Nombre del Tipo de Película"
+                        required
+                    />
+                </div>
+
+
+                {/* SEGUNDA FILA: Descripción */}
+                <div className="col-12">
+                    <FormTextArea
+                        label="Descripción"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        placeholder="Escribe una breve descripción del tipo de película..."
+                        rows={4}
+                    />
+                </div>
+
+            </div>
+        </FormLayout>
+    );
+};
