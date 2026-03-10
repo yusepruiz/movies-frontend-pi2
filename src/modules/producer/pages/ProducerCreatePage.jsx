79ef22af28d136c3540718c9ca1d4afca7ac5bd0
Yusep Ruiz <yusep3012@hotmail.com>
2026-03-10 11:10:31 -0500

    Se crea las páginas que contienen el formulario para crear y actualizar el productor

---
+++ src/modules/producer/pages/ProducerCreatePage.jsx
@@ -0,0 +1,13 @@
+import { ProducerForm } from "@/modules/producer/components/ProducerForm";
+
+/**
+ * Página para la creación de un nuevo productor.
+ * Renderiza el componente ProducerForm en modo creación.
+ * 
+ * @returns {JSX.Element} La página de creación de productores.
+ */
+export const ProducerCreatePage = () => {
+    return (
+        <ProducerForm />
+    );
+}
---
+++ src/modules/producer/pages/ProducerUpdatePage.jsx
@@ -0,0 +1,14 @@
+import { ProducerForm } from "@/modules/producer/components/ProducerForm";
+
+/**
+ * Página para la actualización de un productor existente.
+ * Renderiza el componente ProducerForm configurado para actualización.
+ * 
+ * @returns {JSX.Element} La página de actualización de productores.
+ */
+export const ProducerUpdatePage = () => {
+
+    return (
+        <ProducerForm />
+    );
+};
