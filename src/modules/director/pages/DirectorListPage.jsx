86dbad535d3f33cbd67d214641ef4016f5a1a728
Yusep Ruiz <yusep3012@hotmail.com>
2026-03-10 02:23:25 -0500

    Se crea la página para el listado de los géneros

--- src/modules/director/pages/DirectorListPage.jsx
+++ src/modules/director/pages/DirectorListPage.jsx
@@ -1,37 +1,17 @@
-import { Suspense } from "react";
-import { Link } from "react-router";
-
 import { directorServices } from "@/modules/director/services/directorServices";
-import { DirectorListContent } from "@/modules/director/components/DirectorListContent";
+import { ListLayout } from "@/components/layout/ListLayout";
+import { GenericList } from "@/components/common/GenericList";
 
-/**
- * Componente principal de la página de directores
- * 
- * @returns {JSX.Element}
- */
 export const DirectorListPage = () => {
-    const directorsPromise = directorServices.getDirectors();
+    const directorsPromise = directorServices.getAll();
 
     return (
-        <div className="container mt-4">
-            <div className="d-flex justify-content-between align-items-center mb-4">
-                <h1>Listado de Directores</h1>
-                <span className="badge bg-info text-dark">React 19 Pattern</span>
-            </div>
-
-            <hr />
-
-            <Suspense fallback={
-                <div className="d-flex align-items-center mt-4">
-                    <strong>Cargando directores...</strong>
-                    <div className="spinner-border ms-auto text-primary" role="status" aria-hidden="true"></div>
-                </div>
-            }>
-                <div className="d-flex justify-content-end">
-                    <Link to="/director/create" className="btn btn-sm btn-primary me-4 px-2 py-1" title="Crear"><i className="bi bi-plus me-2"></i>Crear</Link>
-                </div>
-                <DirectorListContent directorsPromise={directorsPromise} />
-            </Suspense>
-        </div>
+        <ListLayout title="Listado de Directores" createLink="/director/create">
+            <GenericList
+                promise={directorsPromise}
+                resourcePath="director"
+                emptyMessage="No hay directores registrados."
+            />
+        </ListLayout>
     );
 };
