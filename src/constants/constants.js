3360894e0bc6f9c4d7060923986c41a9e1dc9b31
Yusep Ruiz <yusep3012@hotmail.com>
2026-03-12 13:44:33 -0500

    Se crean constantes nuevas para los mensajes de las alertas y los colores de las alertas

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,3 +5,18 @@ export const STATE_COLORS = {
     INACTIVE: "#6B7280",
     ACTIVE: "#D97706"
 }
+
+export const ALERTS_MESSAGES = {
+    SUCCESS: "Operación completada con éxito",
+    ERROR: "Ocurrió un error inesperado",
+    DELETE: (displayName) => `Deseas eliminar \"${displayName}\". ¡Esta acción no se puede deshacer!`,
+}
+
+export const ALERTS_BUTTONS_COLORS = {
+    SUCCESS: "#0d6efd",
+    ERROR: "#0d6efd",
+    DELETE: {
+        CONFIRM: "#d33",
+        CANCEL: "#3085d6",
+    },
+}
